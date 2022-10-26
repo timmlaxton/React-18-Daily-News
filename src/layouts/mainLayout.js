86275c87bfbc1f3efaee004a5db6f1b5564f3125
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2022-10-26 11:12:34 +0100

    installed toastiy

--- src/layouts/mainLayout.js
+++ src/layouts/mainLayout.js
@@ -1,4 +1,6 @@
 import { Container } from "react-bootstrap";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const MainLayout = (props) => {
   return <Container className="mt-5 mb-5">{props.children}</Container>;
