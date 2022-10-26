eb5e3eef4e12f9da3099f6e7ec7e15c8c5d8d31f
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2022-10-26 10:59:54 +0100

    added routes to header

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Home from "./components/home";
 import Contact from "./components/contacts";
 import Posts from "./components/posts";
+import Header from "./components/header";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
